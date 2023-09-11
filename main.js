50dd53c373230fb963f9d30e4805e7004e13b789
Ivan Vorobev <vorobev_ivan@icloud.com>
2023-09-11 23:50:16 +0400

    chore: feat: add different rerender strategies

--- main.js
+++ main.js
@@ -1,37 +1,90 @@
+let renderEnj = "posit"
+let animationId;
+const positBtn = document.getElementById('positionRerender')
+positBtn.addEventListener("click", () => {
+    if (renderEnj === "posit") {
+        return
+    }
+    renderEnj = "posit"
+    animateSnowflakes(renderEnj);
+    console.log("positRerender")
+})
+const transBtn = document.getElementById('transformRerender')
+transBtn.addEventListener("click", () => {
+    if (renderEnj === "trans") {
+        return
+    }
+    renderEnj = "trans"
+    animateSnowflakes(renderEnj);
+    console.log("transRerender")
+})
+
+
 function createSnowflake() {
     const snowflake = document.createElement('div');
     snowflake.className = 'snowflake';
     snowflake.style.left = Math.random() * 100 + 'vw';
     document.body.appendChild(snowflake);
 
-    setTimeout(() => {
-        document.body.removeChild(snowflake);
-    }, 5000); // Удалить снежинку через 5 секунд
+    // setTimeout(() => {
+    //     document.body.removeChild(snowflake);
+    // }, 5000); // Удалить снежинку через 5 секунд
 
     return snowflake;
 }
 
-function animateSnowflakes() {
-    const snowflakes = [];
+function positionRerender(snowflakes, currentFlake, currentFlakeIndex) {
+    const yPos = parseFloat(getComputedStyle(currentFlake).top) || 0;
+    currentFlake.style.top = (yPos + 1) + 'px';
+
+    if (yPos > window.innerHeight) {
+        document.body.removeChild(currentFlake);
+        snowflakes.splice(currentFlakeIndex, 1);
+    }
+
+}
+
+function transformRerender(snowflakes, currentFlake) {
+    const yPos = parseFloat(getComputedStyle(currentFlake).transform.split(',')[5]) || 0;
+    const newYPos = yPos + 1;
+    if (newYPos > window.innerHeight) {
+        document.body.removeChild(currentFlake);
+        snowflakes.splice(snowflakes.indexOf(currentFlake), 1);
+    } else {
+        currentFlake.style.transform = `translateY(${newYPos}px)`;
+    }
+}
+
+
+function animateSnowflakes(enj) {
+    let snowflakes = [];
 
     function animate() {
         const snowflake = createSnowflake();
         snowflakes.push(snowflake);
 
         snowflakes.forEach((flake, index) => {
-            const yPos = parseFloat(getComputedStyle(flake).top) || 0;
-            flake.style.top = (yPos + 1) + 'px';
-
-            if (yPos > window.innerHeight) {
-                document.body.removeChild(flake);
-                snowflakes.splice(index, 1);
+            if (enj === 'posit') {
+                positionRerender(snowflakes, flake, index);
+            } else {
+                transformRerender(snowflakes, flake);
             }
         });
 
-        requestAnimationFrame(animate);
+        animationId = requestAnimationFrame(animate); // Запоминаем идентификатор анимации
     }
 
-    animate();
+    // Останавливаем предыдущую анимацию, если она есть
+    if (animationId) {
+        cancelAnimationFrame(animationId);
+        const snowflakesCollection = document.querySelectorAll(".snowflake");
+        snowflakesCollection.forEach(function(snowflake) {
+            snowflake.remove();
+        });
+        snowflakes = [];
+    }
+
+    animate(enj);
 }
 
-animateSnowflakes();
+animateSnowflakes(renderEnj);
